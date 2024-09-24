ecd69da698bdd14d232b65e12cc84dcf053d35ad
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-09-24 13:39:35 +0200

    entire pipeline

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
 import numpy as np
 import mne
-from sklearn.decomposition import FastICA
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
 
 mne.set_log_level(verbose='WARNING')
 
-raw = mne.io.read_raw_edf('files/S001/S001R03.edf', preload=True)
+def load_data(file):
+    raw = mne.io.read_raw_edf(file, preload=True)
 
-# FILTER
+    # FILTER
 
-lo_cut = 0.1
-hi_cut = 30
+    lo_cut = 0.1
+    hi_cut = 30
 
-raw_filt = raw.copy().filter(lo_cut, hi_cut)
-raw_filt = raw_filt.notch_filter(freqs=50.0) # remove line noise
+    filtered = raw.copy().filter(lo_cut, hi_cut)
+    filtered = filtered.notch_filter(freqs=50.0) # remove line noise
 
+    return filtered
+
+raw_filt = load_data("files/S001/S001R03.edf")
 
 # EXTRACT FEATURES
 
@@ -26,6 +33,7 @@ ica = mne.preprocessing.ICA(method="infomax")
 
 def get_features(epochs, lofreq, hifreq, epoch_type):
     feat_mat = []
+    y = []
     for idx, epoch in enumerate(epochs):
         mean = np.mean(epoch, axis=0)
 
@@ -37,11 +45,17 @@ def get_features(epochs, lofreq, hifreq, epoch_type):
         energy = np.sum(activation ** 2, axis=1)
         power = energy / (len(epoch) * sfreq)
 
-        event_type = epochs.events[idx][2]
+        event_type = epochs.events[idx][2] - 1
 
-        features = np.hstack((mean, energy, power, epoch_type, event_type))
+        features = np.hstack((mean, energy, power))
+        y.append(event_type)
+
         feat_mat.append(features)
-    return feat_mat
+    return np.array(feat_mat), np.array(y)
+
+def standardize(arr):
+    scaler = StandardScaler()
+    return scaler.fit_transform(arr)
 
 def get_all_features(data):
     event_id = {"T1": 1, "T2": 2}
@@ -58,13 +72,23 @@ def get_all_features(data):
     ica.fit(erds)
     ica.fit(mrcp)
 
-    ers_feats = get_features(erss, 8, 30, 1)
-    eds_feats = get_features(erds, 8, 30, 2)
-    mrcp_feats = get_features(erds, 3, 30, 3)
+    ers_feats, ers_y = get_features(erss, 8, 30, 1)
+    eds_feats, eds_y = get_features(erds, 8, 30, 2)
+    mrcp_feats, mrcp_y = get_features(erds, 3, 30, 3)
 
-    return ers_feats, eds_feats, mrcp_feats
+    return ers_feats, ers_y, erss.info
 
-a, b, c = get_all_features(raw_filt)
+a, b, info = get_all_features(raw_filt)
+
+scaler = StandardScaler()
+pca = PCA()
+reg = LogisticRegression(penalty='l1', solver='liblinear')
+
+pipeline = Pipeline([("Scaler", scaler), ("CSP", pca), ("LogisticRegression", reg)])
+
+pipeline.fit(a, b)
+
+predict = mne.io.read_raw_edf("files/S002/S002R03.edf", preload=True)
 #TODO
 #   extract features
 #   normalize data
